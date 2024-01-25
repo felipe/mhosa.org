3f53b34fd28ef44b061722b084bb3bfac147eb20
Felipe Oduardo Sierra <420001+felipe@users.noreply.github.com>
2024-01-25 09:15:08 -0700

    season-start

---
+++ src/components/SiteHeader/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const headerMenu = [
+    { text: "Tracks", link: "/tracks" },
+    { text: "Standings", link: "/cart" },
+    { text: "Rules", link: "/rules" },
+    { text: "Associates", link: "/cart" },
+    { text: "About Us", link: "/cart" }
+]
+
+export default function SiteHeader() {
+    return <>
+        <div className="z-10 w-full flex-row font-mono text-sm lg:flex justify-center">
+            <div className="w-full max-w-5xl p-6 text-center"></div>
+        </div>
+        <div className="z-10 w-full flex-row font-mono text-sm lg:flex justify-center">
+            <div className="flex w-full max-w-5xl items-center justify-between bg-sky-500 p-6">
+                <div>
+                    <span><strong>MHOSA</strong></span>
+                    <ul className="inline is-lined">{headerMenu.map((item, i) => <li key={i}><a href={item.link}>{item.text}</a></li>)}</ul>
+                </div>
+                <div className="fixed bottom-0 left-0 flex w-full justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
+                </div>
+            </div>
+        </div>
+    </>
+}
